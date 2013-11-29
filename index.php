c80c48e22cc80c262f67af5d19a599d37e6f7a7b
Anthony Galligani <anthony.galligani@gmail.com>
2013-11-29 12:53:23 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,257 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta >
+<link rel='stylesheet' href='css/960_12_col.css' type='text/css' media='screen, print'>
+
+<style>
+  @import url(http://fonts.googleapis.com/css?family=Average+Sans);
+  
+body {
+  font-family: 'Average Sans', sans-serif;
+  font-style: normal;
+  font-size: 18px;
+  background-color: #f9fdff;
+  background-color: #fafafd;        
+  color: #596266;
+  margin: 0;
+  padding: 0;
+} 
+    
+#main-wrapper {
+  background-color: #9bc;      
+  background-color: #fff;        
+}
+    
+section.preheader {
+  height: 40px;
+  width: 100%;
+  background-color: #9bc;
+  background-color: #00293D;      
+} 
+       
+.main-logo {      
+}  
+     
+header#main-header {
+  margin-top: 16px;
+  margin-bottom: 16px;
+  font-family: 'Average Sans', sans-serif;
+  font-style: normal;
+  font-size: 38px;
+} 
+    
+#main-menu {
+  height: 62px;    
+  background-color: #fff;
+  border-bottom: solid 1px #333;
+}
+
+#main-menu ul {
+  list-style: none;
+  float: right;
+}
+    
+#main-menu ul li {
+  float: left;
+  margin-left: 48px;
+}
+    
+#main-menu ul li a {
+}
+    
+#main-menu ul li a:hover {
+  color: #589;
+}
+        
+#col1, #col2 {
+  float: left;
+}
+
+#col1 {
+  font-family: serif;
+  font-style: normal;
+  font-size: 16px;
+  color: #444; 
+}
+    
+#col2 {
+  margin-top: 25px;
+}
+
+#search-bar {
+/*   background-color: #00293D; */
+   margin-top: 5px;
+   height: 45px;
+   background-color: #596266;
+/*    background-color: #00293D;    */
+   background-color: #DFF5FF;
+   background-color: #FFF6DD;
+/*    border: solid 1px #807B6F; */
+}
+
+#search-bar form {
+  background-color: #222;
+  padding: 10px;
+  height: 25px;
+}
+
+#search-bar form input {
+  float: right;
+  border: 
+}
+#sidebar-left {
+}
+#sidebar-right {
+/* 
+  background: #2672AE;
+ */
+    font-size: 0.9em;
+/* 
+  border-radius: 12px;
+ */
+}
+
+#sidebar-right-block-1, 
+#sidebar-right-block-2, 
+#sidebar-right-block-3 
+{
+    background-color: #596266;
+    color: #f9fdff;
+  padding: 10px;
+  margin-bottom: 10px;
+}
+#sidebar-right-block-1 header,
+#sidebar-right-block-2 header,
+#sidebar-right-block-3 header {
+  height: 35px;
+  color: #DFF5FF;
+  font-size: 1.2em;
+}
+.gradient-bg-1 {
+   /* fallback/image non-cover color */
+   background-color: #ffffff; 
+
+   /* fallback image */
+   background-image: url(images/fallback-gradient.png); 
+
+   /* Safari 4+, Chrome 1-9 */
+   background-image: -webkit-gradient(linear, 0% 0%, 0% 100%, from(#efefef), to(#ffffff));
+
+   /* Safari 5.1+, Mobile Safari, Chrome 10+ */
+   background-image: -webkit-linear-gradient(top, #efefef, #ffffff); 
+
+   /* Firefox 3.6+ */
+   background-image: -moz-linear-gradient(top, #ffecbc, #ffffff);
+ 
+   /* IE 10+ */
+   background-image: -ms-linear-gradient(top, #efefef, #ffffff);
+
+   /* Opera 11.10+ */
+   background-image: -o-linear-gradient(top, #efefef, #ffffff);
+}
+
+footer {
+   background-color: #807B6F;
+   height: 220px;
+}
+.icons  {
+  float: right;
+  margin-right: 216px;
+}
+
+.icons a {
+  display: inline-block;
+  width: 24px;
+  height: 24px;
+  margin: 7px;
+  vertical-align: middle;
+  -o-transition: all 0s;
+  -moz-transition: all 0s;
+  -webkit-transition: all 0s;
+  -ms-transition: all 0s;
+}
+
+.icons a.facebook { background: url('images/social-icons/facebook.png') left top no-repeat; margin-right: 0px;
+} 
+.icons a.twitter { background: url('images/social-icons/twitter.png') left top no-repeat;}
+.icons a.rss { background: url('images/social-icons/rss.png') left top no-repeat; }
+.icons a.linkedin { background: url('images/social-icons/linkedin.png') left top no-repeat; }
+.icons a.email { background: url('images/social-icons/email.png') left top no-repeat; }
+ 
+.icons a:hover { background-position: left -34px; }
+  </style>
+  <script src="script.js"></script>
+  </head>
+  
+<body class="front">
+  <section class="container preheader">
+    <div class="icons">
+    <a class="facebook" href=""></a>
+    <a class="twitter" href=""></a>
+    <a class="linkedin" href=""></a>
+    <a class="email" href=""></a>
+   </div>
+
+  </section>
+
+  <div id='main-wrapper' class="container_12">	  	
+      <nav id='main-menu' class="grid_12">
+      <ul class='grid_5'>
+        <li><a>Home</a></li>
+        <li><a>About</a></li>
+        <li><a>Portfolio</a></li>
+        <li><a>Blog</a></li>
+      </ul>
+    </nav>
+  <section id='search-bar' class="grid_12">
+  <form>
+  <input class="grid_4"></input>
+  </form>
+  </section>
+
+  <div id='col1' class="grid_8">
+          <section style='padding: 10px; margin-top: 25px;'>
+
+        <header id="main-header" class="section section-header">
+      Maecenas faucibus mollis interdum.
+	  </header>
+
+     Web development is both an individual challenge and a community effort. Many are those who have given so much of themselves to exploring and developing this medium of communication and informational exploration. Like those would clear a path for others to follow through the wilderness, leaving cairns and trail markers long the way for others to follow, many technical blogs written by knowledgable geeks have served as invaluable resources and helped many, including myself, from getting lost and guided them to their own destinations.<br/>
+
+   I started this page in part to present my portfolio and also to communicate insights that I have gained into Drupal over the past 3 years of working with this wonderfully interesting CMS, as well as to document snippets of code or function definitions in PHP, jQuery, Javascript, GIT, CSS, HTML and probably other languages to serve as reminders to others and myself. I have also documented other aspects of web development as much for my own benefit as for the benefit of others that we may all share our experiences and knowledge and learn.<br/>
+   
+<br/>  
+</section>
+</div> <!-- -->
+  <div id='col2' class="grid_4">
+    <div id='sidebar-right'>
+      <div id='sidebar-right-block-1'>
+        <header>Drupal Core:</header>
+        <p>Drupal 6, Drupal 7, Drupal 8, hook_page_alter, hook_node_delete, Form API</p>
+     </div> <!-- end sidebar-right-block-1 -->
+      <div id='sidebar-right-block-2'>
+        <header>Contrib Modules:</header>
+        <p>Views, Panels, Field Collections</p>
+     </div> <!-- end sidebar-right-block-1 -->
+      <div id='sidebar-right-block-3'>      
+        <header>jQuery:</header>
+        <p>Drupal 6, Drupal 7, Drupal 8, hook_page_alter, hook_node_delete, CSS, SASS, Linux, jQuery, Views, HTML5</p>
+     </div> <!-- end sidebar-right-block-1 -->
+      <div id='sidebar-right-block-3'>
+        <header>Topics:</header>
+        <p>Drupal 6, Drupal 7, Drupal 8, hook_page_alter, hook_node_delete, CSS, SASS, Linux, jQuery, Views, HTML5</p>
+     </div> <!-- end sidebar-right-block-1 -->
+    </div> <!-- end sidebar-left -->
+  </div>
+  	<div class="grid_12"><p>940px</p></div>
+	<div class="clear"></div>
+	<div class="grid_6" style='color: #999; background-color: #efefef;'><p>460px</p></div>  
+	<div class="grid_6" style='color: #999; background-color: #efefef; margin-bottom: 10px;'><p>460px</p></div>  
+	<div class="clear"></div>
+  </div>
+  <footer>
+  </footer>
+  </body>
+  
+</html>
